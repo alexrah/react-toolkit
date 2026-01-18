0f2245cdf72fb86f1859b732fdd7b6411534ff18
alessandro stoppato <alexrah@gmail.com>
2026-01-18 23:11:10 +0100

    src/utils/ui.ts: tw helper function to trigget tailwind LSP on-demand

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+/**
+ * Helper to pass to LSP tailwind-language-server classFunctions settings to make it recognize & autocomplete tailwind classes
+ * @see https://github.com/tailwindlabs/tailwindcss-intellisense#tailwindcssclassattributes
+ * @usage const className = tw\`bg-red-500`
+ */
+export function tw(strings: TemplateStringsArray, ...values: unknown[]): string {
+  return strings.reduce((result, str, i) => result + str + (values[i] || ''), '');
+}
