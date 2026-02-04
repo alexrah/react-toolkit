a545411b43df9c164c09681d9af1207198e13fb9
alessandro stoppato <alexrah@gmail.com>
2026-02-04 12:46:07 +0100

    src/utils/ui.ts: add better JSDoc for tw() explaining how it works

--- src/utils/ui.ts
+++ src/utils/ui.ts
@@ -7,6 +7,8 @@ export function cn(...inputs: ClassValue[]) {
 
 /**
  * Helper to pass to LSP tailwind-language-server classFunctions settings to make it recognize & autocomplete tailwind classes
+ * @description IMPORTANT: LSP needs to be configured to recognize tw()
+ * settings.tailwindCSS.classFunctions = ["tw"]
  * @see https://github.com/tailwindlabs/tailwindcss-intellisense#tailwindcssclassattributes
  * @usage const className = tw\`bg-red-500`
  */
